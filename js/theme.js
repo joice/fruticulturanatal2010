c1d3dde008c046966a8ace46d36b979ae7afd5fb
Joice Taciana <joicetaciana@gmail.com>
2010-03-09 13:42:01 -0300

    delete the countdown part

--- js/theme.js
+++ js/theme.js
@@ -2,7 +2,4 @@ jQuery(document).ready(function($){
   $(".slideshow").cycle({
     fx: 'fade'
   });
-  $("a[href^='http:']").not("[href*='localhost']").attr('target','_blank');
-  var difference = (new Date("10/17/2010") - new Date()) / 1000;
-  $("#countdown").html(Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)));
 });
