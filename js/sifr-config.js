4dcb26296375ede3482d7dec013e756baac8140c
Joice Taciana <joicetaciana@gmail.com>
2010-03-12 15:13:05 -0300

    change the path ways of the flash on the sifr configuration

--- js/sifr-config.js
+++ js/sifr-config.js
@@ -1,6 +1,6 @@
-var kozuka_el = { src: 'flash/kozuka_gothic_pro_el.swf' };
-var kristen_itc = { src: 'flash/kristen_itc.swf' };
-var officina_bold = { src: 'flash/officina_sans_std_bold.swf' };
+var kozuka_el = { src: '../flash/kozuka_gothic_pro_el.swf' };
+var kristen_itc = { src: '../flash/kristen_itc.swf' };
+var officina_bold = { src: '../flash/officina_sans_std_bold.swf' };
 sIFR.activate(kozuka_el,officina_bold, kristen_itc);
 
 
