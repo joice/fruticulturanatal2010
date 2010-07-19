f8e1c71200ea26e1242040b80c8c0473f1a33761
Anderson Dias <andersondaraujo@gmail.com>
2010-07-19 18:53:36 -0300

    Lista de convidados confirmados

--- _helpers.rb
+++ _helpers.rb
@@ -34,6 +34,24 @@ module Helpers
     tag('img', options)
   end
 
+def guest_detail(name, id, state, items, image = 'default.png')
+    result = ''
+    if state
+      result += tag('img', :src => "images/flags/#{state}.gif", :class => 'flag')
+    else
+      result += tag('img', :src => "images/flags/default.gif", :class => 'flag') 
+    end
+    name_and_flag = name
+    name_and_flag += ' / ' + state unless state.blank?
+    if items.empty?
+      result += content_tag('span', name_and_flag)
+    else
+      result += content_tag('a', name_and_flag, :href => "#guest_#{id}", :class => 'guest_name')
+    end
+
+    content_tag('li', result)
+  end
+
   protected
   def static_url(base_url)
     path = File.join("#{File.expand_path(File.dirname(__FILE__))}", base_url)
