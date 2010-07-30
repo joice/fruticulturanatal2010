6fb004919bfc8e61f2bcaea1f45a8e43a5639c00
Max e Paulo <pfagiani@gmail.com>
2010-07-30 12:38:19 -0300

    create the structure to fancybox to show the guest's descriptions

--- _helpers.rb
+++ _helpers.rb
@@ -48,11 +48,47 @@ def guest_detail(name, id, state, items, image = 'default.png')
     else
       result += content_tag('a', name_and_flag, :href => "#guest_#{id}", :class => 'guest_name')
     end
-
+    
+	  detail_result = ''
+    detail_result += tag('img', :src => "images/guests/#{image}", :class => 'photo')
+    detail_result += guest_description_list(name, items)
+    detail_result = content_tag('div', detail_result, :id => "guest_#{id}")
+    result += content_tag('div', detail_result, :class => 'guest_detail')
+	
     content_tag('li', result)
   end
 
   protected
+  def guest_description_list(name, items)
+    result = ''
+    result += content_tag('li', name, :class => 'name')
+    unless items.empty?
+      items.each do |item|
+        result += content_tag('li', item)
+      end
+    end
+    content_tag 'ul', result
+  end
+
+  def css_tag(href, options = {})
+    options[:rel] ||= 'stylesheet'
+    options[:type] ||= 'text/css'
+    options[:href] = static_url("/css/#{href}")
+    options[:media] ||= 'screen'
+    tag('link', options)
+  end
+
+  def script_tag(source, options = {})
+    options[:src] = static_url("/js/#{source}")
+    options[:type] ||= 'text/javascript'
+    content_tag('script', '', options)
+  end
+
+  def image_tag(source, options = {})
+    options[:src] = static_url("/images/#{source}")
+    tag('img', options)
+  end
+
   def static_url(base_url)
     path = File.join("#{File.expand_path(File.dirname(__FILE__))}", base_url)
     if File.exist?(path)
@@ -62,3 +98,4 @@ def guest_detail(name, id, state, items, image = 'default.png')
     end
   end
 end
+
